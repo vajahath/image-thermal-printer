ddac32ca17d6974af643360ede58420fb19efef6
Vajahath Ahmed <vajuoff.1@gmail.com>
2024-08-03 21:48:16 +0530

    demo correction

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,6 +1,6 @@
 import { generatePrintCommandsForImage } from '@vaju/image-thermal-printer';
 import { autoLoadPrinter, getPrinter, printerInfo$ } from './usb-printer-provider';
-import { fromEvent, lastValueFrom, map, startWith } from 'rxjs';
+import { fromEvent, map, shareReplay, startWith, switchMap } from 'rxjs';
 
 document.addEventListener('DOMContentLoaded', async () => {
   await autoLoadPrinter();
@@ -19,7 +19,8 @@ if (!imageSourceInputElement) throw new Error('Unable to detect source image inp
 
 const imageSource$ = fromEvent(imageSourceInputElement, 'input').pipe(
   map(event => (event?.target as any).value as string),
-  startWith(imageSourceInputElement.value)
+  startWith(imageSourceInputElement.value),
+  shareReplay(1)
 );
 
 imageSource$.subscribe(src => {
@@ -40,22 +41,20 @@ if (!imagePrintButtonEl) throw new Error('No image print button');
 
 const imagePrintOnClick$ = fromEvent(imagePrintButtonEl, 'click');
 
-imagePrintOnClick$.subscribe(async () => {
+imagePrintOnClick$.pipe(switchMap(() => imageSource$)).subscribe(async src => {
   const printer = await getPrinter();
   if (!printer) {
     alert('Printer not authorized / connected');
     return;
   }
 
-  const src = await lastValueFrom(imageSource$);
-
   const printContent = await generatePrintCommandsForImage(src, {
     cutAfterPrint: true,
-    newLinesAfterImage: 3,
+    newLinesAfterImage: 1,
     printerWidthInPx: 377,
   });
 
-  console.log('Prepared print data. Starting to print', printContent);
+  console.log('Prepared print data. Starting to print');
 
   await printer.transferOut(3, printContent);
   console.log('Print completed');
